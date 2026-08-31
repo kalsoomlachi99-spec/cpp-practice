092101eabf18be03c2c13e7422cd8f1ef2ff8ef3
kalsoomlachi99-spec <kalsoomlachi99@gmail.com>
2026-08-31 11:18:32 +0500

    input and output in character array

--- strings/character-array.cpp
+++ strings/character-array.cpp
@@ -10,10 +10,21 @@ int main() {
     char str1[] = "Hello!"; // string literals - literals: constant value
     char str2[] = {'W', 'o', 'r', 'l', 'd', '\0'}; // '\0' is called a NULL character. size of \0 is 1byte, and is count as single character
 
-    cout << "Str1[1] = " << str1[1] << endl;
+    cout << "Str1 = " << str1 << endl;
+    cout << "Str1[0] = " << str1[0] << endl;
 
     cout << "Str2 = " << str2 << endl;
     cout << "Size of str2 = " << strlen(str2) << endl;
+
+    /*Input & Output*/ 
+
+    char str3[100];
+
+    cout << "Enter character array : " ;
+    // cin >> str3; will input only a single word without space
+    cin.getline(str3, 100);
+
+    cout << "Output : " << str3 << endl;
 
     return 0;
 }
